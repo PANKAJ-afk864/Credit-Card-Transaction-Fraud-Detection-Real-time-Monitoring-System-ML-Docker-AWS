c7b1fcdbf085b6ff09ed092c5ad96256f1784e40
Pankaj Kumar Singh <your-email@example.com>
2025-07-05 15:08:34 +0530

    commit

--- app.py
+++ app.py
@@ -35,5 +35,6 @@ def predict():
         return jsonify({"error": str(e)})
 
 # Run the Flask app
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8501, debug=False, use_reloader=False)
+
